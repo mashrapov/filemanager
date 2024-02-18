d88539e2ce332050246d424686d72bf5cf3cb03b
Murat Mashrapov <murat.mashrapov96@gmail.com>
2024-02-18 11:32:08 +0600

    fix

---
+++ database/migrations/2024_02_18_083135_change_path_type_in_files.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePathTypeInFiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            $table->text('path')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('files', function (Blueprint $table) {
+            //
+        });
+    }
+}
